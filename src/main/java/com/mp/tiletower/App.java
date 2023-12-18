2136084500f8e70b00ae9717592add1ce76facb4
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-18 19:20:16 +0200

    Update frame title

--- src/main/java/com/mp/tiletower/App.java
+++ src/main/java/com/mp/tiletower/App.java
@@ -16,6 +16,7 @@ public class App extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("TileTower (0.1.0)");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
