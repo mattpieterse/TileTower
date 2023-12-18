c7582e137c4a05d767105b703b6e9299da89fdae
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-18 21:18:03 +0200

    Update gameplay backend

--- src/main/java/com/mp/tiletower/Game.java
+++ src/main/java/com/mp/tiletower/Game.java
@@ -3,6 +3,18 @@ package com.mp.tiletower;
 
 public class Game {
     
+    static int highest = 0;
+    
+    int score = 0;
+    boolean[][] states = {
+        {false, false},
+        {false, false},
+        {false, false},
+        {false, false},
+        {false, false}
+    };
+    
+    // --
     
     boolean verifyTurn() {
         return false;
