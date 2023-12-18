3efa797d1342e286054217e23254683c30e99d9c
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-18 19:21:59 +0200

    Update frame size

--- src/main/java/com/mp/tiletower/App.java
+++ src/main/java/com/mp/tiletower/App.java
@@ -23,11 +23,11 @@ public class App extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 340, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 460, Short.MAX_VALUE)
         );
 
         pack();
